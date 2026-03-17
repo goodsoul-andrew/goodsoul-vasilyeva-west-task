ecfc1ac92edc9f96a20a51a8bd3be1881b58652c
Andrew Goodman <goodsoul.andrew@mail.ru>
2026-03-17 19:03:25 +0500

    сделал 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import Card from './Card.js';
-import Game from './Game.js';
-import TaskQueue from './TaskQueue.js';
-import SpeedRate from './SpeedRate.js';
+import Card from "./Card.js";
+import Game from "./Game.js";
+import TaskQueue from "./TaskQueue.js";
+import SpeedRate from "./SpeedRate.js";
 
 // Отвечает является ли карта уткой.
 function isDuck(card) {
@@ -16,43 +16,48 @@ function isDog(card) {
 // Дает описание существа по схожести с утками и собаками
 function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
-        return 'Утка-Собака';
+        return "Утка-Собака";
     }
     if (isDuck(card)) {
-        return 'Утка';
+        return "Утка";
     }
     if (isDog(card)) {
-        return 'Собака';
+        return "Собака";
     }
-    return 'Существо';
+    return "Существо";
 }
-
-
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
-}
+class Duck extends Card {
+    constructor() {
+        super("Мирная Утка", 2);
+    }
 
+    quacks() {
+        console.log("quack");
+    }
+
+    swims() {
+        console.log("float: both;");
+    }
+}
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Card {
+    constructor() {
+        super("Пёс-Бандит", 3);
+    }
 }
-
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
-const banditStartDeck = [
-    new Card('Бандит', 3),
-];
-
+const banditStartDeck = [new Dog()];
 
 // Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
@@ -62,5 +67,5 @@ SpeedRate.set(1);
 
 // Запуск игры.
 game.play(false, (winner) => {
-    alert('Победил ' + winner.name);
+    alert("Победил " + winner.name);
 });
